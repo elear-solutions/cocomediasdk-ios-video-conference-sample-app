a679f34ac77f41f0da0ec525a3b983e70d687bce
rohan-elear <rohansahay@elear.solutions>
2021-11-16 15:32:20 +0530

    Setup buttons and actions

--- sessions/ViewController.swift
+++ sessions/ViewController.swift
@@ -15,7 +15,58 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    self.setup()
     
+  }
+  
+  private func setup() {
+    setupButtons()
+  }
+  
+  private func setupVideoButton() {
+    btnEnableVideo.addTarget(self,
+                             action: #selector(buttonDidTap),
+                             for: .touchUpInside)
+    btnEnableVideo.setImage(UIImage(systemName: "video.circle"),
+                            for: .normal)
+    btnEnableVideo.setImage(UIImage(systemName: "video.circle.fill"),
+                            for: .selected)
+  }
+  
+  private func setupMicrophoneButton() {
+    btnEnableMicrophone.addTarget(self,
+                                  action: #selector(buttonDidTap),
+                                  for: .touchUpInside)
+    btnEnableMicrophone.setImage(UIImage(systemName: "mic.circle"),
+      for: .normal)
+    btnEnableMicrophone.setImage(UIImage(systemName: "mic.circle.fill"),
+      for: .selected)
+  }
+  
+  private func setupSpeakerButton() {
+    btnEnableSpeaker.setImage(UIImage(systemName: "speaker.wave.2.circle.fill"),
+      for: .normal)
+    btnEnableSpeaker.setImage(UIImage(systemName: "speaker.slash.circle.fill"),
+      for: .selected)
+    btnEnableSpeaker.addTarget(self,
+                               action: #selector(buttonDidTap),
+                               for: .touchUpInside)
+  }
+  
+  private func setupButtons() {
+    setupVideoButton()
+    setupMicrophoneButton()
+    setupSpeakerButton()
+  }
+  
+  @objc private func buttonDidTap(sender: UIButton) {
+    sender.isSelected = !sender.isSelected
+    switch sender {
+      case btnEnableVideo:
+        debugPrint("\(sender) tapped")
+      default:
+        break
+    }
   }
 }
 
