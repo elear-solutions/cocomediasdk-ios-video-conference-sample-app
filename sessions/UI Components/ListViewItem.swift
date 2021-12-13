fa6f17101158dca2130653eb225d99b1e4cfae09
rohan-elear <rohan-elear@users.noreply.github.com>
2021-12-13 15:22:22 +0530

    Adds ListViewItem

---
+++ sessions/UI Components/ListViewItem.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewItem.swift
+//  sessions
+//
+//  Created by Rohan S on 13/12/21.
+//
+
+import Foundation
+import UIKit
+
+class ListViewItem: UITableViewCell {
+  @IBOutlet var itemLabel: UILabel!
+  @IBOutlet var itemButton: UIButton!
+
+  @IBAction func itemButtonTapped(_: Any) {}
+}
