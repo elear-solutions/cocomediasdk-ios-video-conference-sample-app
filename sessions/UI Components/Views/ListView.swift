73595c0410e4173cf70dd80703fb35cde3ac1f5c
rohan-elear <rohan-elear@users.noreply.github.com>
2021-12-14 15:09:41 +0530

    Adds ListView

--- sessions/UI Components/Views/ListView.swift
+++ sessions/UI Components/Views/ListView.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class ListView: UIView {
+  // Top View
+  @IBOutlet weak var labelWelcome: UILabel!
+  @IBOutlet weak var imageUser: UIImageView!
+  // Middle View
+  @IBOutlet weak var tableListView: UITableView!
+  // Bottom View
+  @IBOutlet weak var actionButton: UIButton!
+  
+  @IBAction func buttonTapped(_ sender: Any) {
+  }
+  
+  func fill() {
+    
+  }
+}
