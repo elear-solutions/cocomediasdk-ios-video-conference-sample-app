7c52f48e4979768e5f7fe7dde43f5836dc8bdb60
rohan-elear <rohan-elear@users.noreply.github.com>
2021-12-16 12:49:27 +0530

    Adds UI items

--- sessions/UI Components/Views/CallView.swift
+++ sessions/UI Components/Views/CallView.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+
+class CallView: UIView {
+  @IBOutlet weak var callerPreview: PreviewView!
+  @IBOutlet weak var callPreview02: UIView!
+  @IBOutlet weak var callPreview03: UIView!
+  @IBOutlet weak var callPreview04: UIView!
+  
+  @IBOutlet weak var btnToggleCamera: UIButton!
+  @IBOutlet weak var btnToggleVideo: UIButton!
+  @IBOutlet weak var btnEndCall: UIButton!
+  @IBOutlet weak var btnToggleMicrophone: UIButton!
+  @IBOutlet weak var btnToggleSpeaker: UIButton!
+  
+  @IBAction private func didTouchUpInside(_ sender: UIButton) {
+    
+  }
+  
+}
