108e311d1c7b1212ad6c665785e7789e84c48f1b
rohan-elear <rohan-elear@users.noreply.github.com>
2021-12-22 13:42:50 +0530

    Adds logic to change rootViewController

--- sessions/AppDelegate.swift
+++ sessions/AppDelegate.swift
@@ -12,6 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    if UserDataManager().getUserLoggedIn() == true {
+      let vc = SessionListViewController.initFromNib()
+      let nav = UINavigationController(rootViewController: vc)
+      let window = UIWindow(frame: UIScreen.main.bounds)
+      window.rootViewController = nav
+      window.makeKeyAndVisible()
+    }
     return true
   }
 
