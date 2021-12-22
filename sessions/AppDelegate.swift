1e3fc6a793a94578e4ced98f9131de28a046455e
rohan-elear <rohan-elear@users.noreply.github.com>
2021-12-22 15:42:31 +0530

    Update AppDelegate.swift

--- sessions/AppDelegate.swift
+++ sessions/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import CocoMediaSDK
 
 @available(iOS 13.0, *)
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    // Initialize CocoMediaSDK
+    let config = CocoMediaConfig(clientId: <#T##String#>, cwdPath: <#T##String#>)
+    // Set RootViewController
     if UserDataManager().getUserLoggedIn() == true {
       let vc = SessionListViewController.initFromNib()
       let nav = UINavigationController(rootViewController: vc)
