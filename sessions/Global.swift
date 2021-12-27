bbde1fe9550a53d4d8e71a5ac4ab97494c2069e5
rohan-elear <53178851+rohan-elear@users.noreply.github.com>
2021-12-27 13:06:45 +0530

    Update Global.swift

--- sessions/Global.swift
+++ sessions/Global.swift
@@ -5,8 +5,11 @@
 //  Created by Rohan S on 20/12/21.
 //
 
+import CocoMediaSDK
 import UIKit
 
 enum StoryBoard {
   static let Main = UIStoryboard(name: "Main", bundle: nil)
 }
+
+public var client: CocoMediaClient?
