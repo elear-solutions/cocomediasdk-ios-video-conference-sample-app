c7ed95c82e232b90c925aaed3c052311ff55a0b8
rohan-elear <53178851+rohan-elear@users.noreply.github.com>
2022-01-09 18:35:23 +0530

    Adds API call

--- sessions/UI Components/Views/CreateSessionViewController.swift
+++ sessions/UI Components/Views/CreateSessionViewController.swift
@@ -23,5 +23,32 @@ class CreateSessionViewController: UIViewController {
 
   @IBAction func didTapButton(_ sender: Any) {
     debugPrint(sender)
+    guard let networkName = txtSessionName.text else {
+      let alert = UIAlertController(title: "Error Encountered",
+                                    message: "Please enter a name",
+                                    preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "Dismiss",
+                                    style: .default,
+                                    handler: nil))
+      self.present(alert, animated: true, completion: nil)
+      return
+    }
+    showSpinner(onView: self.view)
+    NetworkService().createNetworkApi(
+      networkName: networkName,
+      success: { networkId in
+        self.removeSpinner()
+        self.dismiss(animated: true)
+      },
+      failure: { error in
+        self.removeSpinner()
+        let alert = UIAlertController(title: "Error Encountered",
+                                      message: error.localizedDescription,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss",
+                                      style: .default,
+                                      handler: nil))
+        self.present(alert, animated: true, completion: nil)
+      })
   }
 }
